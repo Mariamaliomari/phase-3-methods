57ae4f675c464bbba85f784a4ff62f2cfb77d2cd
Omari99 <mariam.ali01@student.moringaschool.com>
2023-02-06 19:25:59 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,42 @@
 # Your code here!
+def greet_programmer (name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  greet_programmer
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+  end
+  greet ("Naureen")
+
+
+
+  def greet_with_default (name = "Rubyist")
+    puts "Hello, #{name}!"
+  end
+ greet_with_default "Naureen"
+  
+  
+  def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  greet_with_default 
+
+
+  def add (num1, num2)
+    return num1 + num2
+  end
+   add(2, 2)
+
+
+   def halve(numb1,numb2)
+    return numb1/ numb2
+    end
+    halve(6/2)
+
+    def halve(numb1,numb2)
+        return numb1/numb2
+        end
+        halve(6, "5")
+
